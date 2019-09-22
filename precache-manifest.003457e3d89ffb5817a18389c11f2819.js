644d56bab45c9162e59fc8dab949260adc9198da
unknown <abanoubmaher2015@gmail.com>
2019-09-22 12:45:47 +0200

    Updates

---
+++ precache-manifest.003457e3d89ffb5817a18389c11f2819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbe4a7882b8ccdd46e2801ec6404620",
+    "url": "/reminder-app/index.html"
+  },
+  {
+    "revision": "33b90205fdde17833949",
+    "url": "/reminder-app/static/css/2.cb0a7b97.chunk.css"
+  },
+  {
+    "revision": "c01e222b049ab2411be3",
+    "url": "/reminder-app/static/css/main.9804f010.chunk.css"
+  },
+  {
+    "revision": "33b90205fdde17833949",
+    "url": "/reminder-app/static/js/2.22f7bd9a.chunk.js"
+  },
+  {
+    "revision": "c01e222b049ab2411be3",
+    "url": "/reminder-app/static/js/main.429a7095.chunk.js"
+  },
+  {
+    "revision": "23ca9e594317deb856a3",
+    "url": "/reminder-app/static/js/runtime-main.6dad33f0.js"
+  }
+]);
